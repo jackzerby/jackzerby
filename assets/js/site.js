f3002cd55fbcfd81a2f3af10c20719f05324578d
Jack Zerby <jack@hiidef.com>
2013-06-01 16:50:12 -0400

    first commit

---
+++ assets/js/site.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  //Event Tracking
+  $('.js-ga-event').each(function() {
+    $(this).click(function(){
+      ga('send', 'event', $(this).data("category"), $(this).data("action"), $(this).data("label"));
+      return false;
+    });
+  });
+});
