6cc4fdef123580d4be68e4dc9e88713d87b7a4bf
Jack Zerby <jack@hiidef.com>
2013-06-04 10:06:38 -0400

    more changes

--- _site/assets/js/site.js
+++ _site/assets/js/site.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     $(this).click(function(){
       _gaq.push(['_trackEvent', $(this).data("category"), $(this).data("action"), $(this).data("label")]);
       return false;
-      alert("hello");
     });
   });
 });
