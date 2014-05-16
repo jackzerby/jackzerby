202dd910f4b1393a553a7da3575f3e7d2763cb75
Jack Zerby <jack@hiidef.com>
2014-05-16 19:43:15 -0400

    changes

--- _site/assets/js/site.js
+++ _site/assets/js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  //Event Tracking
+  // Event Tracking
   $('.js-ga-event').each(function() {
     $(this).click(function(){
       ga('send', 'event', $(this).data("category"), $(this).data("action"), $(this).data("label"));
